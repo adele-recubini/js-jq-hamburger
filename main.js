9f28e9724f844043f36b6c7ae2721eded60742a7
adele <adele.r87@gmail.com>
2021-01-12 15:40:23 +0100

    hamburger  menu

--- main.js
+++ main.js
@@ -2,10 +2,10 @@
 // utilizzo show  sul selettore  appropiatoal primo click per vedere la lista
 // utilizzo hide sul selettore approrpiato in questo caso una x (close) al secondo click per togliere la lista ( non farla vedere)
 
-$('.fas fa-bars').click(function() {
-  $('.hamburger-menu ul li').show("slow");
+$('.header-right').click(function() {
+  $('.hamburger-menu').show("slow");
 })
 
 $('.close').click(function() {
-  $('.hamburger-menu ul li').hide("fast");
+  $('.hamburger-menu').hide("fast");
 })
