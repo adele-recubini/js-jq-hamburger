0c168846d8b7ce6df63bcc940e5fc5a7ebb3a12c
adele <adele.r87@gmail.com>
2021-01-12 16:08:34 +0100

    hamburger-menu + bonus

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
 // voglio che la classe hamburger menu al mio click faccia apparire la li lista degli ul
-// utilizzo show  sul selettore  appropiatoal primo click per vedere la lista
-// utilizzo hide sul selettore approrpiato in questo caso una x (close) al secondo click per togliere la lista ( non farla vedere)
-
+// utilizzo show  sul selettore giusto come primo click per vedere la lista
+// utilizzo hide sul selettore in questo caso una x (close) al secondo click per togliere la lista ( non farla vedere)
+// inserisco show e hide per dire che voglio una comparla lenta nel primo caso e veloce nel secondo
+//
 $('.header-right').click(function() {
   $('.hamburger-menu').show("slow");
 })
 
+//
+
 $('.close').click(function() {
   $('.hamburger-menu').hide("fast");
+})
+
+// BONUS fadeIn e fadeOut
+
+$('.header-right').click(function() {
+  $('.hamburger-menu').fadeIn("slow");
+})
+
+//
+
+$('.close').click(function() {
+  $('.hamburger-menu').fadeOut("fast");
 })
